fb9a1d4d87ecfeeafc1b4c3da02b8c2be6d12efd
bc17 <bc17@inf.ufpr.br>
2021-01-10 11:19:23 -0300

    fixed the selected line with click

--- T3/script.js
+++ T3/script.js
@@ -24,7 +24,7 @@ var startX,startY;
 var selected;
 var lines=[];
 
-lines.push({x0:75, y0:25, x1:200,y1:25});
+lines.push({x0:75, y0:25, x1:350,y1:25});
 draw();
 
 //mouse events inside canvas
@@ -37,84 +37,81 @@ canvas.addEventListener('mouseout', handleMouseUpOut);
 
 //generate polygon based on corners chose by user
 function generatePolygon (){
+
   var corners = parseInt(document.getElementById("corners").value);
   if(corners == 0 )
     return;
-
   lines=[];
 
-  //angle of points based on number of corners choosen
-  var radius = 1;
+  //angle of pts based on number of corners choosen
   var angle = (Math.PI * 2) / corners;
 
   // points
-  var points = [];
+  var pts = [];
+  var radius = 1;
+
   for (var i=0; i<corners; i++) {
       var a = angle * i;
-      //sin and cos are swithced,point 0 is bottom one
       var x = (Math.sin(a)*radius);
       var y = (Math.cos(a)*radius);
-      points.push({
+      pts.push({
         x:x,
         y:y
       })
       //add lines
-      lines.push(({}))
+      lines.push(({}));
   }
-  //get the angle of a side
-  var sideangle = Math.atan2(points[1].y-points[0].y, points[1].x-points[0].x)
-  var nPoints = points.length
-  //rotate point around bottom one
-  for (var i=1; i<nPoints; i++)
-  {
-    points[i] = {
-        x:Math.cos(sideangle) * (points[i].x - points[0].x) - Math.sin(sideangle) * (points[i].y-points[0].y) + points[0].x,
-        y:Math.sin(sideangle) * (points[i].x - points[0].x) + Math.cos(sideangle) * (points[i].y - points[0].y) + points[0].y
+  //angle of a side
+  var sideangle = Math.atan2(pts[1].y-pts[0].y, pts[1].x-pts[0].x);
+  var nPoints = pts.length;
+
+  //rotate point based on the bottom one
+  for (var i=1; i<nPoints; i++){
+    pts[i] = {
+        x:Math.cos(sideangle) * (pts[i].x - pts[0].x) - Math.sin(sideangle) * (pts[i].y-pts[0].y) + pts[0].x,
+        y:Math.sin(sideangle) * (pts[i].x - pts[0].x) + Math.cos(sideangle) * (pts[i].y-pts[0].y) + pts[0].y
     };
   }
 
-  var rect = {top:2,left:2,right:-2,bottom:-2};
+  //rectangle size
+  var rect = {left:2,top:2,right:-2,bottom:-2};
   for (var i=0; i<nPoints; i++){
-    rect.top = Math.min(rect.top,points[i].y);
-    rect.bottom = Math.max(rect.bottom,points[i].y);
-    rect.left = Math.min(rect.left,points[i].x);
-    rect.right = Math.max(rect.right,points[i].x);
+    rect.left = Math.min(rect.left,pts[i].x);
+    rect.top = Math.min(rect.top,pts[i].y);
+    rect.bottom = Math.max(rect.bottom,pts[i].y);
+    rect.right = Math.max(rect.right,pts[i].x);
   }
-  rect.width = Math.abs(rect.right - rect.left);
-  rect.height = Math.abs(rect.bottom - rect.top);
+  rect.width = difference(rect.right,rect.left);
+  rect.height = difference(rect.bottom,rect.top);
 
-  //make points relative to top left of rect
+  //points relative to top left of rect
   for (var i=0; i<nPoints; i++){
-      points[i] = {
-          x: points[i].x - rect.left
-          ,y: points[i].y - rect.top
+      pts[i] = {
+          x: pts[i].x - rect.left,
+          y: pts[i].y - rect.top
       };
   }
 
-  //scale and position the poly based on its rect
-  var ratioX = cw / rect.width;
-  var ratioY = cw / rect.height;
-  var ratio = Math.min(ratioX, ratioY);
-
-  var i=0
-  for (i=0; i<nPoints; i++){
-    points[i] = {
-      x: (points[i].x * ratio),
-      y: (points[i].y * ratio)
+  //scale and position the polygon based on the rectangle
+  var ratio = Math.min((cw / rect.width), (cw / rect.height));
+  for (var i=0; i<nPoints; i++){
+    pts[i] = {
+      x: (pts[i].x * ratio),
+      y: (pts[i].y * ratio)
     };
   }
 
-  //transforme points in lines
-  lines[0].x0 = points[nPoints-1].x;
-  lines[0].y0 = points[nPoints-1].y;
-  lines[0].x1 = points[0].x;
-  lines[0].y1 = points[0].y;
+  //transforme pts in lines
+  lines[0].x0 = pts[nPoints-1].x;
+  lines[0].y0 = pts[nPoints-1].y;
+  lines[0].x1 = pts[0].x;
+  lines[0].y1 = pts[0].y;
 
-  for (i=1; i<nPoints; i++){
-    lines[i].x0 = points[i-1].x;
-    lines[i].y0 = points[i-1].y;
-    lines[i].x1 = points[i].x;
-    lines[i].y1 = points[i].y;
+  for (var i=1; i<nPoints; i++){
+    lines[i].x0 = pts[i-1].x;
+    lines[i].y0 = pts[i-1].y;
+    lines[i].x1 = pts[i].x;
+    lines[i].y1 = pts[i].y;
   }
   //draw lines
   draw();
@@ -145,7 +142,7 @@ function difference(a, b) {
 }
 
 
-// find which section of the line the mouse clicked, in the points or middle
+// find which section of the line the mouse clicked, in the pts or middle
 function whichSection(line,mx,my){
   console.log(mx, my, line)
   var smaller, bigger;
